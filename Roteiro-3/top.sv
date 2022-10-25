926a8e9dbbc5b6c833ca362e51a632bc3a8de0e7
Charles Bezerra de Oliveira Junior <charlesbezerra5@gmail.com>
2022-10-24 22:56:40 -0300

    new file:   Roteiro-2/top.sv 	new file:   Roteiro-3/top.sv 	new file:   top.sv

---
+++ Roteiro-3/top.sv
@@ -0,0 +1,65 @@
+// Aluno: Charles Bezerra de Oliveira Júnior - 119110595
+// Roteiro 3
+
+parameter divide_by=100000000;  // divisor do clock de referência
+// A frequencia do clock de referencia é 50 MHz.
+// A frequencia de clk_2 será de  50 MHz / divide_by
+
+parameter NBITS_INSTR = 32;
+parameter NBITS_TOP = 8, NREGS_TOP = 32, NBITS_LCD = 64;
+module top(input  logic clk_2,
+           input  logic [NBITS_TOP-1:0] SWI,
+           output logic [NBITS_TOP-1:0] LED,
+           output logic [NBITS_TOP-1:0] SEG,
+           output logic [NBITS_LCD-1:0] lcd_a, lcd_b,
+           output logic [NBITS_INSTR-1:0] lcd_instruction,
+           output logic [NBITS_TOP-1:0] lcd_registrador [0:NREGS_TOP-1],
+           output logic [NBITS_TOP-1:0] lcd_pc, lcd_SrcA, lcd_SrcB,
+             lcd_ALUResult, lcd_Result, lcd_WriteData, lcd_ReadData, 
+           output logic lcd_MemWrite, lcd_Branch, lcd_MemtoReg, lcd_RegWrite);
+
+  always_comb begin
+    SEG <= SWI;
+    lcd_WriteData <= SWI;
+    lcd_pc <= 'h12;
+    lcd_instruction <= 'h34567890;
+    lcd_SrcA <= 'hab;
+    lcd_SrcB <= 'hcd;
+    lcd_ALUResult <= 'hef;
+    lcd_Result <= 'h11;
+    lcd_ReadData <= 'h33;
+    lcd_MemWrite <= SWI[0];
+    lcd_Branch <= SWI[1];
+    lcd_MemtoReg <= SWI[2];
+    lcd_RegWrite <= SWI[3];
+    for(int i=0; i<NREGS_TOP; i++)
+       if(i != NREGS_TOP/2-1) lcd_registrador[i] <= i+i*16;
+       else                   lcd_registrador[i] <= ~SWI;
+    lcd_a <= {56'h1234567890ABCD, SWI};
+    lcd_b <= {SWI, 56'hFEDCBA09876543};
+  end
+
+  logic [1:0] F;
+  logic signed [2:0] A, B;
+
+  always_comb begin
+    A <= SWI[7:5];
+    B <= SWI[2:0];
+    F <= SWI[4:3];
+
+    unique case (F) 
+    endcase
+
+    if (F[0] & F[1]) 
+      LED[2:0] <= A | B;
+
+    else if (~F[1] & F[0]) 
+      LED[2:0] <= A - B;
+
+    else if (F[1] & ~F[0]) 
+      LED[2:0] <= A & B;
+
+    else 
+      LED[2:0] <= A + B;
+  end
+endmodule
