d2638d52e8d6b4f07d814e3b732011a4f1e271b0
Charles Bezerra <Charles@MacBook-Pro-de-ACT.local>
2022-12-01 08:14:01 -0300

    new file:   Roteiro-5/top.sv 	modified:   top.sv

--- Roteiro-5/top.sv
+++ Roteiro-5/top.sv
@@ -1,5 +1,5 @@
-// Charles Bezerra de Oliveira Júnior - 119110595
-// Roteiro 5
+// DESCRIPTION: Verilator: Systemverilog example module
+// with interface to switch buttons, LEDs, LCD and register display
 
 parameter divide_by=100000000;  // divisor do clock de referência
 // A frequencia do clock de referencia é 50 MHz.
@@ -19,7 +19,7 @@ module top(input  logic clk_2,
            output logic lcd_MemWrite, lcd_Branch, lcd_MemtoReg, lcd_RegWrite);
 
   always_comb begin
-    // SEG <= SWI;
+    SEG <= SWI;
     lcd_WriteData <= SWI;
     lcd_pc <= 'h12;
     lcd_instruction <= 'h34567890;
@@ -40,48 +40,10 @@ module top(input  logic clk_2,
   end
 
   logic [3:0] contador;
-  logic reset
-  logic decrescente;
-  logic has_load;
-  logic [3:0] data_in;
+  logic reset;
 
   always_comb reset <= SWI[0];
-  always_comb decrescente <= SWI[1];
-  always_comb has_load <= SWI[2];
-  always_comb data_in <= SWI[7:4];
 
-  parameter APAGADO = 'b00000000;
-  parameter ZERO = 'b00111111;
-  parameter UM = 'b00000110;
-  parameter DOIS = 'b01011011;
-  parameter QUATRO = 'b01011011;
-  parameter CINCO = 'b01011011;
-  parameter SEIS = 'b01011011;
-  parameter SETE = 'b01011011;
-  parameter OITO = 'b01011011;
-  parameter NOVE = 'b01011011;
-  parameter A = 'b01011011;
-  parameter B = 'b01011011;
-  parameter C = 'b01011011;
-  parameter E = 'b01011011;
-  parameter F = 'b01011011;
-
-  always_ff @(posedge clk_2 or posedge reset) begin
-    if (reset)
-      contador <= 0;
-    else if (has_load)
-      contador <= data_in;
-    else 
-      if (decrescente)
-        contador <= contador-1;
-      else 
-        contador <= contador+1;
+  always_ff @(posedge clk_2) begin
   end
-
-  always_comb begin
-    
-    
-  end
-
-  always_comb LED[7] <= clk_2;
 endmodule
